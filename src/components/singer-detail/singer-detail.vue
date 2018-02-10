c481290d91b8723984f43e84bcb7675d2029d8c8
gr <510071555@qq.com>
2018-02-10 18:13:58 +0800

    music-list组件实现及优化

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
 <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
 </transition> 
 </template>
 <script>
@@ -8,6 +8,7 @@
     import {getSingerDetail} from 'api/singer'
     import {ERR_OK} from 'api/config'
     import {createSong} from 'common/js/song'
+    import MusicList from 'components/music-list/music-list'
     export default {
         data () {
             return {
@@ -15,6 +16,12 @@
             }
         },
         computed: {
+            title() {
+                return this.singer.name
+            },
+            bgImage() {
+                return this.singer.avatar
+            },
             ...mapGetters([
                 'singer'
             ])
@@ -46,6 +53,9 @@
                 })
                 return ret
             }
+        },
+        components: {
+            'music-list': MusicList
         }
     }
 </script>
